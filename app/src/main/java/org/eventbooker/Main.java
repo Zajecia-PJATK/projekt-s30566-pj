8272e849449e99d541971c185f95d61a1e7aabb6
s30566 <s30566@pjwstk.edu.pl>
2024-06-08 21:28:59 +0200

    Poprawa struktury plików

--- app/src/main/java/org/eventbooker/Main.java
+++ app/src/main/java/org/eventbooker/Main.java
@@ -1,5 +1,7 @@
+package org.eventbooker;
+
 public class Main {
     public static void main(String[] args){
-        System.out.println("Witaj świecie");
+        System.out.println("Hello World");
     }
 }
