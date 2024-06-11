c77f44895f2fbacdec0de268f351e6c8c015f4f0
s30566 <s30566@pjwstk.edu.pl>
2024-06-11 20:27:35 +0200

    Added driver for mysql connection

---
+++ EventBooker/src/main/java/pj/s30566/MysqlDriver.java
@@ -0,0 +1,33 @@
+package pj.s30566;
+import java.sql.*;
+
+public class MysqlDriver {
+
+    private String url;
+    private String user;
+    private String password;
+
+    public MysqlDriver(String url, String user, String password){
+        this.url = url;
+        this.user = user;
+        this.password = password;
+    }
+
+    public Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(url, user, password);
+    }
+
+    public ResultSet executeQuery(String query) throws SQLException{
+        Connection connection = getConnection();
+        Statement statement = connection.createStatement();
+        return statement.executeQuery(query);
+    }
+
+    public int executeUpdate(String query) throws SQLException {
+        Connection connection = getConnection();
+        Statement statement = connection.createStatement();
+        return statement.executeUpdate(query);
+    }
+
+
+}
