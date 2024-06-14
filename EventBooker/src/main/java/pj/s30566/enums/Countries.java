e95ab0762ef93d517d3d384e5a00b5fa7782998d
s30566 <s30566@pjwstk.edu.pl>
2024-06-15 00:01:22 +0200

    Added enums: Roles, EventType, EventStatus, Countries

---
+++ EventBooker/src/main/java/pj/s30566/enums/Countries.java
@@ -0,0 +1,32 @@
+package pj.s30566.enums;
+
+public enum Countries {
+    AUSTRIA,
+    BELGIUM,
+    BULGARIA,
+    CROATIA,
+    CYPRUS,
+    CZECH_REPUBLIC,
+    DENMARK,
+    ESTONIA,
+    FINLAND,
+    FRANCE,
+    GERMANY,
+    GREECE,
+    HUNGARY,
+    IRELAND,
+    ITALY,
+    LATVIA,
+    LITHUANIA,
+    LUXEMBOURG,
+    MALTA,
+    NETHERLANDS,
+    POLAND,
+    PORTUGAL,
+    ROMANIA,
+    SLOVAKIA,
+    SLOVENIA,
+    SPAIN,
+    SWEDEN
+
+}
