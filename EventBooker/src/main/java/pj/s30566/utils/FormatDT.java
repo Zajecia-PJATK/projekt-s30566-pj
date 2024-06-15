ff3ae3c6076438f83c342fa2251972f96780899b
s30566 <s30566@pjwstk.edu.pl>
2024-06-15 22:11:06 +0200

    Added DateTime converter

---
+++ EventBooker/src/main/java/pj/s30566/utils/FormatDT.java
@@ -0,0 +1,12 @@
+package pj.s30566.utils;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class FormatDT {
+    private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+
+    public String format(LocalDateTime time){
+        return time.format(formatter);
+    }
+}
