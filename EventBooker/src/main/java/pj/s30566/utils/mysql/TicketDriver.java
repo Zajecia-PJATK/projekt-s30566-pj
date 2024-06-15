6f7a156e726ec70ddbbd1b844a4eae0bc2d9b971
s30566 <s30566@pjwstk.edu.pl>
2024-06-15 18:23:27 +0200

    Added TicketDriver

---
+++ EventBooker/src/main/java/pj/s30566/utils/mysql/TicketDriver.java
@@ -0,0 +1,33 @@
+package pj.s30566.utils.mysql;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import pj.s30566.model.Location;
+import pj.s30566.model.Ticket;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class TicketDriver extends MysqlDriver {
+    private static final Logger logger = LoggerFactory.getLogger(LocationDriver.class);
+    private static String INSERT_TICKET_SQL = "INSERT INTO Locations (event_id, user_id, seat_number, purchase_date, status) VALUES (?, ?, ?, ?, ?)";
+    public TicketDriver(){
+
+    }
+
+    public void addTicket(Ticket ticket) throws SQLException {
+        try (PreparedStatement preparedStatement = super.getConnection().prepareStatement(INSERT_TICKET_SQL)){
+            preparedStatement.setString(1, ticket.getEventId());
+            preparedStatement.setString(2, ticket.getUserId());
+            preparedStatement.setString(3, ticket.getSeatNumber());
+            preparedStatement.setString(4, ticket.getPurchaseDate().toString());
+            preparedStatement.setString(5, ticket.getStatus().name());
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            logger.error("An error occurred while adding ticket", e);
+        }
+    }
+
+
+
+}
