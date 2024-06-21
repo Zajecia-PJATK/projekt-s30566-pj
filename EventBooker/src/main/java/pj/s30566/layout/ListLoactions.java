8770dbc7f55ed500360d714cdcfbca673b79b1a3
s30566 <s30566@pjwstk.edu.pl>
2024-06-21 13:48:00 +0200

    Now you can add locations

---
+++ EventBooker/src/main/java/pj/s30566/layout/ListLoactions.java
@@ -0,0 +1,127 @@
+package pj.s30566.layout;
+
+import pj.s30566.model.Event;
+import pj.s30566.model.Location;
+import pj.s30566.model.user.User;
+import pj.s30566.utils.mysql.EventDriver;
+import pj.s30566.utils.mysql.LocationDriver;
+
+import java.io.Console;
+import java.io.IOException;
+import java.util.List;
+import java.util.Scanner;
+
+public class ListLoactions {
+
+    private final Scanner scanner = new Scanner(System.in);
+    public Location listLocations(User user) {
+        LocationDriver locationDriver = new LocationDriver();
+        System.out.println("Twoje lokacje: ");
+        List<Location> locations = locationDriver.getLocationsByUser(user.getID());
+        int elementCount = locations.size();
+        int index = 0;
+        Console console = System.console();
+        char input;
+        while (true){
+            System.out.println("Obiekt: " + (index + 1) + "/" + elementCount + " q - wyjdź, a - poprzedni element  , d - następny element");
+            System.out.println();
+            System.out.println("Nazwa: " + locations.get(index).getLocationName());
+            System.out.println("Adres: " + locations.get(index).getAddress());
+            System.out.println(locations.get(index).getPostalCode() + "\t" + locations.get(index).getCity() + "\t");
+            System.out.println(locations.get(index).getCountry().name());
+            System.out.println("ID lokacji: " + locations.get(index).getLocationID());
+            System.out.println();
+            System.out.println("Wpisz 'e' aby wybrac te lokacje");
+
+            if (console != null){
+                try {
+                    input = (char) console.reader().read();
+                } catch (IOException e){
+                    System.out.println("Problem z konsolą.");
+                    input = scanner.next().charAt(0);
+                }
+                if (input == 'a'){
+                    index = (index - 1 + locations.size()) % locations.size();
+                } else if (input == 'd'){
+                    index = (index + 1) % locations.size();
+                } else if (input == 'e'){
+                    return locations.get(index);
+                } else if (input == 'q') {
+                    break;
+                } else {
+                    System.out.println("Zły przycisk!");
+                }
+            } else {
+                input = scanner.next().charAt(0);
+                if (input == 'a'){
+                    index = (index - 1 + locations.size()) % locations.size();
+                } else if (input == 'd'){
+                    index = (index + 1) % locations.size();
+                } else if (input == 'e'){
+                    return locations.get(index);
+                } else if (input == 'q') {
+                    break;
+                } else {
+                    System.out.println("Zły przycisk!");
+                }
+            }
+        }
+        return null;
+    }
+
+    public Location.Venue selectVenue(Location location){
+        LocationDriver locationDriver = new LocationDriver();
+        System.out.println("Sale w " + location.getLocationName());
+        List<Location.Venue> venues = locationDriver.getVenuesByLocationID(location.getLocationID());
+        int elementCount = venues.size();
+        int index = 0;
+        Console console = System.console();
+        char input;
+        while (true){
+            System.out.println("Obiekt: " + (index + 1) + "/" + elementCount + " q - wyjdź, a - poprzedni element  , d - następny element");
+            System.out.println();
+            System.out.println("ID: " + venues.get(index).getVenueId());
+            System.out.println("Nazwa: " + venues.get(index).getVenueName());
+            System.out.println("Pojemnosc: : " + venues.get(index).getCapacity());
+            System.out.println();
+            System.out.println("Wpisz 'e' aby wybrac te lokacje");
+
+            if (console != null){
+                try {
+                    input = (char) console.reader().read();
+                } catch (IOException e){
+                    System.out.println("Problem z konsolą.");
+                    input = scanner.next().charAt(0);
+                }
+                if (input == 'a'){
+                    index = (index - 1 + venues.size()) % venues.size();
+                } else if (input == 'd'){
+                    index = (index + 1) % venues.size();
+                } else if (input == 'e'){
+                    return venues.get(index);
+                } else if (input == 'q') {
+                    break;
+                } else {
+                    System.out.println("Zły przycisk!");
+                }
+            } else {
+                input = scanner.next().charAt(0);
+                if (input == 'a'){
+                    index = (index - 1 + venues.size()) % venues.size();
+                } else if (input == 'd'){
+                    index = (index + 1) % venues.size();
+                } else if (input == 'e'){
+                    return venues.get(index);
+                } else if (input == 'q') {
+                    break;
+                } else {
+                    System.out.println("Zły przycisk!");
+                }
+            }
+        }
+
+        return null;
+    }
+
+
+}
--- EventBooker/src/main/java/pj/s30566/utils/event/EventManager.java
+++ EventBooker/src/main/java/pj/s30566/utils/event/EventManager.java
@@ -1,18 +1,93 @@
 package pj.s30566.utils.event;
 
+import pj.s30566.enums.Countries;
+import pj.s30566.enums.EventStatus;
+import pj.s30566.enums.EventType;
+import pj.s30566.layout.ListLoactions;
 import pj.s30566.model.Event;
+import pj.s30566.model.Location;
+import pj.s30566.model.user.User;
+import pj.s30566.utils.mysql.EventDriver;
 import pj.s30566.utils.output.Wipe;
 
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 
 public class EventManager {
     Scanner scanner = new Scanner(System.in);
+    ListLoactions listLoactions = new ListLoactions();
+    NumberFormat priceFormat = new DecimalFormat("#0.00");
+    EventDriver eventDriver = new EventDriver();
 
-    public void createNewEvent(Event event){
+    public void createNewEvent(User user){
         Wipe.wipe();
         System.out.println("=== Nowe Wydarzenie ===");
-        System.out.println();
-        System.out.println();
+        System.out.println(/*pusta linia*/);
+        System.out.println("Podaj nazwę wydarzenia: ");
+        String eventName = scanner.nextLine();
+        System.out.println("W jakiej lokacji odbywa sie wydarzenie?");
+        Location location = this.listLoactions.listLocations(user);
+        System.out.println("W jakiej sali odbywa sie wydarzenie?");
+        Location.Venue venue = this.listLoactions.selectVenue(location);
+        System.out.println("Podaj cene biletu: ");
+        double price = scanner.nextDouble();
+        price = Double.parseDouble(priceFormat.format(price));
+        System.out.println("Podaj liczbe miejsc (0 jeśli liczba miejsc rowna sie pojemnosci sali: ");
+        int capacity = scanner.nextInt();
+        if (capacity == 0){
+            capacity = venue.getCapacity();
+        }
+        System.out.println("Kiedy sie odbedzie?");
+        System.out.println("Podaj czas w nastepujacym formacie: YYYY-MM-DD hh:mm:ss");
+        String timeString = scanner.nextLine();
+        LocalDateTime time = LocalDateTime.parse(timeString, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+        System.out.println("Podaj typ wydarzenia: ");
+        EventType eventType = selectEventType();
+        EventStatus eventStatus = EventStatus.SCHEDULED;
+
+        Event event = new Event(price, user.getID(), location.getLocationID(), venue.getVenueId(), eventName, capacity, time, eventStatus, eventType);
+
+        eventDriver.addEvent(event);
+
+    }
+
+    public EventType selectEventType() {
+        boolean isChosen = false;
+        EventType chosenEvent;
+        do {
+            EventType[] eventTypes = EventType.values();
+            int columns = 3;
+
+            for (int i = 0; i < eventTypes.length; i++) {
+                System.out.print("[" + i + "]" + eventTypes[i] + "\t\t");
+                if ((i + 1) % columns == 0) {
+                    System.out.println();
+                }
+            }
+
+            if (eventTypes.length % columns != 0) {
+                System.out.println();
+            }
+
+            System.out.println("Wybierz kraj, podajac jego numer: ");
+            int choice = scanner.nextInt();
+
+
+            if (choice >= 0 && choice < eventTypes.length) {
+                chosenEvent = eventTypes[choice];
+                isChosen = true;
+            } else {
+                System.out.println("Zly numer!");
+                chosenEvent = null;
+            }
+        } while (!isChosen);
+
+
+
+        return chosenEvent;
     }
 
 }
